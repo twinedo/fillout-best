77d52650c05d6ab2bcb291ddab258c6a0b411a0c
Twin Edo <twinedo.dev@gmail.com>
2025-06-21 21:30:29 +0700

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="https://build.fillout.com/favicon.ico" sizes="any" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
