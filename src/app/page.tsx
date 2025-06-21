0dd989b2beb3dc1ed05b50d18a9551c5fa26443c
Twin Edo <twinedo.dev@gmail.com>
2025-06-21 20:15:27 +0700

    add image and center when first load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { Footer, SelectedScreen } from "@/components";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-[#faf2d9]">
+      <Image src={"/fillout-logo-dark.png"} alt="logo" width={115} height={30} />
       <SelectedScreen />
       <Footer />
     </div>
--- src/components/selected-screen/index.tsx
+++ src/components/selected-screen/index.tsx
@@ -5,20 +5,20 @@ export const SelectedScreen = () => {
   const selectedMenu = useMenuStore((state) => state.selectedMenu);
 
   const baseClassName = `
-    flex flex-col justify-center w-full border-[0.5px] border-[#9DA4B259] rounded-2xl gap-[32px] row-start-2 items-center sm:items-start
+    flex flex-col justify-center w-full gap-[32px] row-start-2 items-center sm:items-start
   `;
 
   return (
     <main className={baseClassName}>
       {selectedMenu && (
-        <div className="flex justify-center items-center w-full min-h-[480px] ">
+        <div className="flex justify-center items-center w-full min-h-[60vh] border-[0.5px] border-[#9DA4B259] rounded-2xl">
           <div className="text-7xl">
             This is page of <h1 className="font-bold">{selectedMenu.text}</h1>
           </div>
         </div>
       )}
       {!selectedMenu && (
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col justify-center w-full items-center gap-4 min-h-[60vh]">
           <div className="text-2xl font-bold">Welcome to Fillout</div>
           <div className="text-lg text-gray-600 mb-20">
             Please click &quot;Add Page&quot; below to get started
